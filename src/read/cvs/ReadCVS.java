13f7bd6ba783ad0b486aa365be11eead14172879
marciosn <marciosouzanobre@gmail.com>
2016-01-28 21:04:37 -0300

    calculando média e adicionando coluna

--- src/read/cvs/ReadCVS.java
+++ src/read/cvs/ReadCVS.java
@@ -5,7 +5,10 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.print.attribute.standard.Media;
 
 import org.supercsv.io.CsvListReader;
 import org.supercsv.io.CsvListWriter;
@@ -16,27 +19,36 @@ public class ReadCVS {
 	  public static void main(String[] args) {
 
 			ReadCVS obj = new ReadCVS();
-			obj.run("C:/enem.csv");
+			try {
+				obj.run("C:/enem.csv");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 	  }
 	
 	
-	 public void run(String pathCSV) {
-
+	 public void run(String pathCSV) throws IOException {
 			String csvFile = pathCSV;
 			BufferedReader br = null;
 			String line = "";
 			String cvsSplitBy = ";";
-
+			String[] result = null;
+			double media = 0.0;
+			
+			List<Double> mediasCalculadas = new ArrayList<Double>();
+			
 			try {
-
+				
 				br = new BufferedReader(new FileReader(csvFile));
 				while ((line = br.readLine()) != null) {
-					String[] enemResult = line.split(cvsSplitBy);
+					result = line.split(cvsSplitBy);
 					
 					// IGNORA A PRIMEIRA LINHA DO CSV QUE NAO CONTEM VALORES, SO OS NOMES DAS COLUNAS
-					if(enemResult[0].contains("inscricao") || enemResult[0].contains("cienciasDaNatureza") || enemResult[0].contains("matematica"))
+					if(result[0].contains("inscricao") || result[0].contains("cienciasDaNatureza") || result[0].contains("matematica"))
 						continue;
 					
+							
 					/*System.out.println("Inscricao =" + enemResult[0] +
 							" ; cienciasDaNatureza =" + enemResult[1] +
 							" ; cienciasHumanas =" + enemResult[2] +
@@ -44,20 +56,38 @@ public class ReadCVS {
 							" ; matematica =" + enemResult[4] +
 							" ; redacao =" + enemResult[5]);*/
 										
-					float coluna_1 = Float.valueOf(enemResult[1]);
-					float coluna_2 = Float.valueOf(enemResult[2]);
-					float coluna_3 = Float.valueOf(enemResult[3]);
-					float coluna_4 = Float.valueOf(enemResult[4]);
-					float coluna_5 = Float.valueOf(enemResult[5]);
 					
-					double media = (coluna_1 + coluna_2 + coluna_3 + coluna_4 + coluna_5) / 5;
+					float coluna_1 = Float.valueOf(result[1]);
+					float coluna_2 = Float.valueOf(result[2]);
+					float coluna_3 = Float.valueOf(result[3]);
+					float coluna_4 = Float.valueOf(result[4]);
+					float coluna_5 = Float.valueOf(result[5]);
 					
-					//System.out.println("Média " + media);
-				
-
+					media = (coluna_1 + coluna_2 + coluna_3 + coluna_4 + coluna_5) / 5;
+					mediasCalculadas.add(media);
+					System.out.println("Média " + media);
 				}
 				
-				adicionaColunaMedia();
+				CsvListReader reader;
+				try {
+					reader = new CsvListReader(new FileReader("C:/enem.csv"), CsvPreference.STANDARD_PREFERENCE);
+					CsvListWriter writer = new CsvListWriter(new FileWriter("C:/Users/Marcio/Videos/saida.csv"), CsvPreference.STANDARD_PREFERENCE);
+					 List<String> columns;
+					 int i = 0;
+					 while ((columns = reader.read()) != null) {				 
+					    // columns.add(1, "	 Media");
+						 columns.add(1, "	  "+String.valueOf(mediasCalculadas.get(i)));
+					     writer.write(columns);
+
+					     i++;
+					     System.out.println(i);
+					 }
+					
+					 reader.close();
+					 writer.close();
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				}
 
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
@@ -82,19 +112,13 @@ public class ReadCVS {
 			reader = new CsvListReader(new FileReader("C:/enem.csv"), CsvPreference.STANDARD_PREFERENCE);
 			CsvListWriter writer = new CsvListWriter(new FileWriter("C:/Users/Marcio/Videos/saida.csv"), CsvPreference.STANDARD_PREFERENCE);
 			 List<String> columns;
-			 while ((columns = reader.read()) != null) {
-			     //System.out.println("Input: " + columns);
-			     // Add new columns
-			     columns.add(1, "Media");
-			     //columns.add("Last_column");
-
-			     //System.out.println("Output: " + columns);
+			 while ((columns = reader.read()) != null) {	 
+			     columns.add(1, "	 Media");
 			     writer.write(columns);
 			 }
 			 reader.close();
 			 writer.close();
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		 
